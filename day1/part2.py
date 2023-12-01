1bb76174d4bdb8ee6e9bc6633b6500c2083fc82e
GabriMac <gabri1998gabri@gmail.com>
2023-12-01 23:23:04 +0100

    solved day1 part2

--- day1/part2.py
+++ day1/part2.py
@@ -3,7 +3,7 @@ input_lines = f.readlines()
 
 lines = []
 for input_line in input_lines:
-    lines.append(str(input_line))
+    lines.append(str(input_line.strip()))
 
 string_numbers_dict = {
     'one': '1',
@@ -16,12 +16,15 @@ string_numbers_dict = {
     'eight': '8', 
     'nine': '9'
 }
+strings_numbers = list(string_numbers_dict.keys())
+
 sum = 0
 for line in lines:
     number_found = False
     first_n = 0
     last_n = 0
-    i = 0
+    i = 1
+    j = 0
     for char in line:
         if char.isdigit():
             if not number_found:
@@ -31,7 +34,22 @@ for line in lines:
             else:
                 last_n = char
         
+        else:
+            if line[i-2].isdigit():
+                pass
+
+            else:
+                if any(substring in line[j:i] for substring in strings_numbers):
+                    num_char = string_numbers_dict[next(substring for substring in strings_numbers if substring in line[j:i])]
+                    if not number_found:
+                        number_found = True
+                        first_n = num_char
+                        last_n = num_char
+                    else:
+                        last_n = num_char
+        
         i = i + 1
+        j = 0 if i - 5 < 0 else i - 5
     
 
     sum = sum + int(first_n+last_n)

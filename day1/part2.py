07e6fda26f197d6af7814befccdc3f6a780fdc5d
Gabriel Romero <gabriel.romero@fundcraft.lu>
2023-12-01 16:35:34 +0100

    added day1 part2

---
+++ day1/part2.py
@@ -0,0 +1,29 @@
+f = open("input.txt", "r")
+input_lines = f.readlines()
+
+lines = []
+for input_line in input_lines:
+    lines.append(str(input_line))
+
+string_numbers = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
+sum = 0
+for line in lines:
+    number_found = False
+    first_n = 0
+    last_n = 0
+    i = 0
+    for char in line:
+        if char.isdigit():
+            if not number_found:
+                number_found = True
+                first_n = char
+                last_n = char
+            else:
+                last_n = char
+        
+        i = i + 1
+    
+
+    sum = sum + int(first_n+last_n)
+    
+print(sum)

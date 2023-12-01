edbd48927ad82c7f226b4583932bdc6f65703535
Gabriel Romero <gabriel.romero@fundcraft.lu>
2023-12-01 15:14:27 +0100

    solved day 1 part 1

--- day1/part1.py
+++ day1/part1.py
@@ -1,6 +1,25 @@
 f = open("input.txt", "r")
-lines = f.readlines()
+input_lines = f.readlines()
 
-numbers = []
+lines = []
+for input_line in input_lines:
+    lines.append(str(input_line))
+
+sum = 0
 for line in lines:
-    numbers.append(int(line))
+    number_found = False
+    first_n = 0
+    last_n = 0
+    for char in line:
+        if char.isdigit():
+            if not number_found:
+                number_found = True
+                first_n = char
+                last_n = char
+            else:
+                last_n = char
+    
+
+    sum = sum + int(first_n+last_n)
+    
+print(sum)
